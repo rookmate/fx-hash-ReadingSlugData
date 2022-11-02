c32b4814c2d15ea24b7f9672608500a9f1c1a4d2
rookmate <14072042+rookmate@users.noreply.github.com>
2022-11-02 18:00:41 +0000

    Split to collectionFloor and tokenListing

--- fx_api.js
+++ fx_api.js
@@ -1,7 +1,8 @@
 const { GraphQLClient, gql } = require('graphql-request');
 
 const collection = "magic-carpet";
-let _listings = {
+const tokenID = 1258939;
+let _output = {
   id: 0,
   price: 0.0,
   symbol: "xxx",
@@ -12,24 +13,73 @@ let _listings = {
   collectionSlug: "",
   rarity: 0,
   traits: [],
-  //contractAddress,
-  //site,
+  contractAddress: "",
+  site: "fx(hash)",
   fromName: "",
-  //fromAddress,
-  //toName,
-  //toAddress,
-  //txHash,
+  fromAddress: "",
+  toName: "",
+  toAddress: "",
+  txHash: "",
   type: "listing",
-  //isPrivate,
+  isPrivate: false,
   amount: 1,
 }
 
-async function activeListingsFromSlug(collectionSlug, _listings) {
+async function tokenListing(tokenID) {
   const endpoint = 'https://api.fxhash.xyz/graphql'
 
   const graphQLClient = new GraphQLClient(endpoint)
 
-  const query = gql`
+  const tokenQuery = gql`
+    query GenerativeToken($objktId: Float) {
+      objkt(id: $objktId) {
+        id
+        slug
+        name
+        displayUri
+        activeListing {
+          price
+        }
+        rarity
+        owner {
+          name
+        }
+        features
+      }
+    }
+  `
+
+  const variables = {
+    objktId: tokenID,
+  }
+
+  //const slugData = await graphQLClient.request(collectionQuery, variables)
+  const tokenData = await graphQLClient.request(tokenQuery, variables)
+  //console.log(JSON.stringify(tokenData, undefined, 2));
+  _output = {
+    price: Number.parseFloat(tokenData.objkt.activeListing.price / (10 ** 6)).toFixed(3),
+    symbol: "XTZ",
+    imageUrl: `https://gateway.fxhash.xyz/ipfs/${tokenData.objkt.displayUri.replace("ipfs://", "")}`,
+    permalink: `https://www.fxhash.xyz/gentk/slug/${tokenData.objkt.slug}`,
+    collectionName: tokenData.objkt.name,
+    collectionSlug: tokenData.objkt.slug,
+    tokenId: tokenData.objkt.id,
+    rarity: tokenData.objkt.rarity,
+    fromName: tokenData.objkt.owner.name,
+    type: "listing",
+    site: "fx(hash)",
+    amount: tokenData.objkt.activeListing.amount || 1,
+    traits: tokenData.objkt.features,
+  }
+  console.log(_output);
+}
+
+async function collectionFloor(collectionSlug) {
+  const endpoint = 'https://api.fxhash.xyz/graphql'
+
+  const graphQLClient = new GraphQLClient(endpoint)
+
+  const collectionQuery = gql`
     query GenerativeToken($slug: String) {
       generativeToken(slug: $slug) {
         id
@@ -37,21 +87,6 @@ async function activeListingsFromSlug(collectionSlug, _listings) {
         name
         marketStats {
           floor
-        }
-        activeListedObjkts {
-          id
-          name
-          slug
-          displayUri
-          rarity
-          activeListing {
-            amount
-            price
-          }
-          owner {
-            name
-          }
-          features
         }
       }
     }
@@ -61,26 +96,20 @@ async function activeListingsFromSlug(collectionSlug, _listings) {
     slug: collectionSlug,
   }
 
-  const slugData = await graphQLClient.request(query, variables)
+  const slugData = await graphQLClient.request(collectionQuery, variables)
   //console.log(JSON.stringify(slugData, undefined, 2));
-  _listings = {
-    id: slugData.generativeToken.id,
-    price: Number.parseFloat(slugData.generativeToken.activeListedObjkts[0].activeListing.price / (10 ** 6)).toFixed(3),
-    symbol: "XTZ",
-    imageUrl: `https://gateway.fxhash.xyz/ipfs/${slugData.generativeToken.activeListedObjkts[0].displayUri}`,
-    permalink: `https://www.fxhash.xyz/gentk/slug/${slugData.generativeToken.activeListedObjkts[0].slug}`,
+  _output = {
     collectionName: slugData.generativeToken.name,
-    tokenId: slugData.generativeToken.activeListedObjkts[0].id,
     collectionSlug: slugData.generativeToken.slug,
-    rarity: slugData.generativeToken.activeListedObjkts[0].rarity,
-    traits: slugData.generativeToken.activeListedObjkts[0].features,
-    fromName: slugData.generativeToken.activeListedObjkts[0].owner.name,
     type: "listing",
-    amount: slugData.generativeToken.activeListedObjkts[0].activeListing.amount || 1,
+    site: "fx(hash)",
+    price: Number.parseFloat(slugData.generativeToken.marketStats.floor / (10 ** 6)).toFixed(3),
+    symbol: "XTZ",
   }
-  console.log(_listings);
+  console.log(_output);
 }
 
-activeListingsFromSlug(collection, _listings).catch((error) => console.error(error));
+collectionFloor(collection).catch((error) => console.error(error));
+tokenListing(tokenID).catch((error) => console.error(error));
 
 // Query tested here: https://studio.apollographql.com/sandbox/explorer?endpoint=https%3A%2F%2Fapi.fxhash.xyz%2Fgraphql&explorerURLState=N4IgJg9gxgrgtgUwHYBcQC4QEcYIE4CeABAOLL4CGKAlgG4IAqEA1sgBQAkAzgDYwDm6IgGUUeakn4BKIsAA6SIkX7k8VOoxbteAodz7TZCpUuphjJnfwtKkFRDaJwKeVilFUuRxSaUAzHggIPEcAX0cKKBp6ABlqLhQEMAB5ACMAK2YUL3kfXzNHW3sEQqIrUrB4gAceCgIAVXFStXEUAlLI6IQ4hIl%2Bb19fKvEoErzB%2BwgYVFLw8aUIAHckfAHBood5ojn1vwQqGDwELjCLOdCQABoQWhdqClSeY4wQXKU5EHKMIg%2BVJABaAAeBAAXv8wBQuP9oLAeCgPpcFBdQkA
